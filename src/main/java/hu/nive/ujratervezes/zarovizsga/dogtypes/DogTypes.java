e7feb5624cd6ea26eff4205b43716b77ff905b0f
szendre1 <endre.szarka@gmail.com>
2021-03-16 20:30:43 +0100

    Kutyafajták

--- src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -12,6 +12,29 @@ public class DogTypes {
 
     public List<String> getDogsByCountry(String country){
 
+
+//        try (Connection conn = dataSource.getConnection();
+//             PreparedStatement stmt = conn.prepareStatement("select * from activities where id=?");
+//        ) {
+//            List<Activity> listfromDB = new ArrayList<>();
+//            //ps.setLong(1, 1L);
+//            stmt.setLong(1, id);
+//            try (ResultSet rs = stmt.executeQuery()) {
+//                while (rs.next()) {
+//                    int idDb = rs.getInt("id");
+//                    LocalDateTime start_time = rs.getTimestamp("start_time").toLocalDateTime();
+//                    String desc = rs.getString("activity_desc");
+//                    String type = rs.getString("activity_type");
+//                    listfromDB.add(new Activity(idDb, start_time, desc, Type.valueOf(type)));
+//                }
+//                return listfromDB;
+//            }
+//        } catch (SQLException e) {
+//            throw new IllegalArgumentException("Cannot insert", e);
+//        }
+
+
+
         return null;
     }
 
